7495b262156adfa919fb773947231fc62933101a
Nursessee <87604826+Nursessee@users.noreply.github.com>
2021-12-19 17:11:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ input_shape=(image_size, image_size, num_channels)))
             padding='same'))
    model.add(MaxPooling2D(pool_size=(2, 2)))
    model.add(Flatten())
-   # Densely connected layers
+
    model.add(Dense(128, activation='relu'))
-   # Output layer
+
    model.add(Dense(num_classes, activation='softmax'))
    model.compile(optimizer=Adam(), loss='categorical_crossentropy',
             metrics=['accuracy'])
@@ -45,27 +45,26 @@ input_shape=(image_size, image_size, num_channels)))
 def mnist_cnn_train(model):
    (train_digits, train_labels), (test_digits, test_labels) = keras.datasets.mnist.load_data()
 
-   # Get image size
-   image_size = 28
-   num_channels = 1  # 1 for grayscale images
 
-   # re-shape and re-scale the images data
+   image_size = 28
+   num_channels = 1
+
+
    train_data = np.reshape(train_digits, (train_digits.shape[0], image_size, image_size, num_channels))
    train_data = train_data.astype('float32') / 255.0
-   # encode the labels - we have 10 output classes
-   # 3 -> [0 0 0 1 0 0 0 0 0 0], 5 -> [0 0 0 0 0 1 0 0 0 0]
+
    num_classes = 10
    train_labels_cat = keras.utils.to_categorical(train_labels, num_classes)
 
-   # re-shape and re-scale the images validation data
+
    val_data = np.reshape(test_digits, (test_digits.shape[0], image_size, image_size, num_channels))
    val_data = val_data.astype('float32') / 255.0
-   # encode the labels - we have 10 output classes
+
    val_labels_cat = keras.utils.to_categorical(test_labels, num_classes)
 
    print("Training the network...")
 
-   # Start training the network
+
    model.fit(train_data, train_labels_cat, epochs=8, batch_size=64,
         validation_data=(val_data, val_labels_cat))
 
